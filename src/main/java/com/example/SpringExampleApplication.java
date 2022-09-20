b79dd273687be85009f6d916f1eaa06f450d48bc
DESKTOP-KHVJGSC\user <0414soyeon@gmail.com>
2022-09-20 21:43:19 +0900

    0920 데이터베이스 연동

--- src/main/java/com/example/SpringExampleApplication.java
+++ src/main/java/com/example/SpringExampleApplication.java
@@ -1,15 +1,34 @@
 package com.example;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+@MapperScan(basePackages = "com.example.*") //db관련 클래스 찾는 annotation
 @SpringBootApplication
 public class SpringExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringExampleApplication.class, args);
 	}
+	
+	// SessionFactory라는 bean을 spring bean으로 만든다.
+	 @Bean
+	    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+	        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+	        sessionFactory.setDataSource(dataSource);
+
+	        Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
+	        sessionFactory.setMapperLocations(res);
+
+	        return sessionFactory.getObject();
+	    }
 
 }
