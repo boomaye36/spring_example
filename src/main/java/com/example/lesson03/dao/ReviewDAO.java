bd4afc5562ddfaf7ad27e06778198215438b86a6
DESKTOP-KHVJGSC\user <0414soyeon@gmail.com>
2022-09-21 21:46:16 +0900

    no message

--- src/main/java/com/example/lesson03/dao/ReviewDAO.java
+++ src/main/java/com/example/lesson03/dao/ReviewDAO.java
@@ -1,5 +1,10 @@
 package com.example.lesson03.dao;
 
-public interface ReviewDAO {
+import org.springframework.stereotype.Repository;
 
+import com.example.lesson03.model.Review;
+
+@Repository
+public interface ReviewDAO {
+	public Review selectReviewById(int id);
 }
